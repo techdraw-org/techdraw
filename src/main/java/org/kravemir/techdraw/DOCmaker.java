09b795783b5939ee2e8ab8e20641ce8f183486ad
Miroslav Kravec <kravec.miroslav@gmail.com>
2016-08-05 19:00:41 +0200

    MultiPage drawing initials

--- src/main/java/org/kravemir/techdraw/DOCmaker.java
+++ src/main/java/org/kravemir/techdraw/DOCmaker.java
@@ -7,38 +7,56 @@ import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Miroslav Kravec
  */
 public class DOCmaker {
 
-    public Document makeDoc(Collection<PartGroup> groups) {
+    public Document[] makeDoc(Collection<PartGroup> groups) {
         String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
         DOMImplementation domImpl = SVGDOMImplementation.getDOMImplementation();
-        Document doc = domImpl.createDocument(svgNS, "svg", null);
 
-        Element svgRoot = doc.getDocumentElement();
+        List<Document> documents = new ArrayList<>();
 
-        // Set the width and height attributes on the root 'svg' element.
-        svgRoot.setAttributeNS(null, "width", "21cm");
-        svgRoot.setAttributeNS(null, "height", "29.7cm");
-        svgRoot.setAttributeNS(null, "viewBox", "0 0 210 297");
+        Document doc = null;
 
-
-        double xoffset = 10, xmax = 210 - xoffset*2;
+        double xoffset = 10, xmax = 210 - xoffset * 2;
         double y = 15;
 
-        for(PartGroup g : groups) {
+        for (PartGroup g : groups) {
+            Element svgRoot;
+            if (doc == null) {
+                doc = domImpl.createDocument(svgNS, "svg", null);
+                documents.add(doc);
+
+                svgRoot = doc.getDocumentElement();
+
+                // Set the width and height attributes on the root 'svg' element.
+                svgRoot.setAttributeNS(null, "width", "21cm");
+                svgRoot.setAttributeNS(null, "height", "29.7cm");
+                svgRoot.setAttributeNS(null, "viewBox", "0 0 210 297");
+
+                y = 15;
+            } else {
+                svgRoot = doc.getDocumentElement();
+            }
+
             GroupDrawer.DrawResult dr = new SimpleGroupDrawer(g).draw(xmax, 9999.0);
             BoxedElement be = dr.getBoxedElement();
-            Element e = be.toSvgElement(doc,svgNS);
-            e.setAttribute("transform", String.format("translate(%f,%f)", xoffset,y));
+            Element e = be.toSvgElement(doc, svgNS);
+            e.setAttribute("transform", String.format("translate(%f,%f)", xoffset, y));
             svgRoot.appendChild(e);
             y += be.getHeight() + 5;
+
+            if (y > 290) {
+                doc = null;
+            }
         }
 
-        return doc;
+        return documents.toArray(new Document[documents.size()]);
     }
 }
--- src/main/java/org/kravemir/techdraw/deskdraw/App.java
+++ src/main/java/org/kravemir/techdraw/deskdraw/App.java
@@ -15,19 +15,46 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * @author Miroslav Kravec
  */
 public class App {
+    static Collection<Desk> parts = Collections.unmodifiableList(Arrays.asList(
+            new Desk(800, 100, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(200, 200, 18, new boolean[] {true,true,true,true}),
+            new Desk(600, 320, 18, new boolean[] {true,true,true,true}),
+            new Desk(800, 200, 24, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(800, 100, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
+            new Desk(200, 200, 18, new boolean[] {true,true,true,true}),
+            new Desk(200, 200, 18, new boolean[] {true,true,true,true}),
+            new Desk(600, 320, 18, new boolean[] {true,true,true,true}),
+            new Desk(800, 200, 24, new boolean[] {true,true,true,true}),
+            new Desk(800, 400, 24, new boolean[] {true,true,true,true}),
+            new Desk(1000, 200, 12, new boolean[] {true,true,true,true}),
+            new Desk(800, 400, 24, new boolean[] {true,true,true,true}),
+            new Desk(1000, 200, 12, new boolean[] {true,true,true,true}),
+            new Desk(320, 210, 12, new boolean[] {true,true,true,true})
+    ));
 
-
-    public static Collection<PartGroup> createDeskGroups(Collection<Desk> desks) {
+    public Collection<PartGroup> createDeskGroups(Collection<Desk> desks) {
         Map<Double,List<Desk>> byWidth = desks.stream().collect(Collectors.groupingBy(Desk::getWidth));
         return byWidth.entrySet().stream().map(entry ->  {
             Collection<BoxedElement> elements = entry.getValue().stream().map(Desk::createElement).collect(Collectors.toList());
@@ -42,60 +69,70 @@ public class App {
         }).collect(Collectors.toList());
     }
 
-    public static void main(String[] args) {
-        Collection<Desk> parts = Arrays.asList(
-                new Desk(800, 100, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(200, 200, 18, new boolean[] {true,true,true,true}),
-                new Desk(600, 320, 18, new boolean[] {true,true,true,true}),
-                new Desk(800, 200, 24, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(800, 100, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(100, 300, 18, new boolean[] {true,true,true,true}),
-                new Desk(200, 200, 18, new boolean[] {true,true,true,true}),
-                new Desk(200, 200, 18, new boolean[] {true,true,true,true}),
-                new Desk(600, 320, 18, new boolean[] {true,true,true,true}),
-                new Desk(800, 200, 24, new boolean[] {true,true,true,true}),
-                new Desk(800, 400, 24, new boolean[] {true,true,true,true}),
-                new Desk(1000, 200, 12, new boolean[] {true,true,true,true}),
-                new Desk(800, 400, 24, new boolean[] {true,true,true,true}),
-                new Desk(1000, 200, 12, new boolean[] {true,true,true,true}),
-                new Desk(320, 210, 12, new boolean[] {true,true,true,true})
-        );
-
+    private SVGDocument[] prepareDocuments() {
         Collection<PartGroup> groups = createDeskGroups(parts);
+        Document[] documents = new DOCmaker().makeDoc(groups);
 
-        Document doc = new DOCmaker().makeDoc(groups);
+        return Arrays.stream(documents)
+                .map(document -> {
+                    try {
+                        DOMSource domSource = new DOMSource(document);
+                        StringWriter writer = new StringWriter();
+                        StreamResult result = new StreamResult(writer);
+                        TransformerFactory tf = TransformerFactory.newInstance();
+                        Transformer transformer = tf.newTransformer();
+                        transformer.transform(domSource, result);
 
-        try {
-            DOMSource domSource = new DOMSource(doc);
-            StringWriter writer = new StringWriter();
-            StreamResult result = new StreamResult(writer);
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer transformer = tf.newTransformer();
-            transformer.transform(domSource, result);
+                        String parser = XMLResourceDescriptor.getXMLParserClassName();
+                        SAXSVGDocumentFactory factory = new SAXSVGDocumentFactory(parser);
+                        return factory.createSVGDocument("", new ByteArrayInputStream(writer.toString().getBytes("UTF-8")));
+                    } catch (TransformerException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .filter(svgDocument -> svgDocument != null)
+                .toArray(SVGDocument[]::new);
+    }
 
-            String parser = XMLResourceDescriptor.getXMLParserClassName();
-            SAXSVGDocumentFactory factory = new SAXSVGDocumentFactory(parser);
-            SVGDocument document = factory.createSVGDocument("", new ByteArrayInputStream(writer.toString().getBytes("UTF-8")));
+    private void run() {
+        JFrame f = new JFrame("TechDraw");
+        f.setSize(1280,800);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+        f.setVisible(true);
 
-            JSVGCanvas c = new JSVGCanvas();
-            c.setSVGDocument(document);
+        SVGDocument[] documents = prepareDocuments();
 
-            JFrame f = new JFrame("TechDraw");
-            f.setSize(1280,800);
-            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.getContentPane().add(c);
-            f.setVisible(true);
-        } catch (TransformerException ex) {
-            throw new RuntimeException(ex);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JSVGCanvas c = new JSVGCanvas();
+        c.setSVGDocument(documents[0]);
+        f.getContentPane().add(c);
+
+        JComboBox<SVGDocument> comboBox = new JComboBox<>(documents);
+        comboBox.setSelectedIndex(0);
+        comboBox.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SVGDocument document = (SVGDocument) comboBox.getSelectedItem();
+                c.setSVGDocument(document);
+            }
+        });
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        bottomPanel.add(new Label("Show page"));
+        bottomPanel.add(comboBox);
+        bottomPanel.setMaximumSize(new Dimension(100000, 64));
+        f.getContentPane().add(bottomPanel, BorderLayout.CENTER);
+
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new App().run();
     }
 }
